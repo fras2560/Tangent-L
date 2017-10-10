942ce0635b66f6fa8b1f4bae276deb6164762340
fras2560 <dallas_j_frase@hotmail.com>
2017-10-10 19:24:43 -0400

    Updated the stripper
    The old one was probable fine

--- src/tangent/htmlStriper.py
+++ src/tangent/htmlStriper.py
@@ -8,15 +8,32 @@ Purpose: Used to strip html for parsing documents
 import unittest
 import re
 from bs4 import BeautifulSoup
+from html.parser import HTMLParser
+
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        super(HTMLParser, self).__init__()
+        self.reset()
+        self.strict = False
+        self.convert_charrefs = True
+        self.fed = []
+
+    def handle_data(self, d):
+        self.fed.append(d)
+
+    def get_data(self):
+        return ' '.join(self.fed)
 
 
 def strip_tags(html):
     """Returns a string stripped of all html tags
     """
-    words = re.sub(r'<.*?>', ' ', html)
-    words = words.replace("  ", " ")
-    return BeautifulSoup(words).text
-
+    parser = HTMLParser()
+    html = parser.unescape(html)
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
 
 
 if __name__ == "__main__":
