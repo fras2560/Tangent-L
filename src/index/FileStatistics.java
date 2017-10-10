84c6b2333b2a74fea804d319ce701f9ba1893c20
fras2560 <dallas_j_frase@hotmail.com>
2017-10-10 19:23:28 -0400

    A class to keep track of file statistics
    Keeps track of number of words, # of tuples, average # size of formulas,
    and max size of formulas

---
+++ src/index/FileStatistics.java
@@ -0,0 +1,73 @@
+package index;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileStatistics {
+    private List<Float> formulas;
+    private float words;
+
+    public FileStatistics(InputStreamReader is) throws IOException{
+        int character;
+        String token = "";
+        boolean math = false;
+        float formulaSize = 0f;
+        this.words = 0;
+        this.formulas = new ArrayList<Float>();
+        while ((character = is.read()) != -1){
+            if (Character.isWhitespace(character)){
+                if (token.startsWith("NUMBER SIGN") && token.endsWith("NUMBER SIGN") && !token.equals("NUMBER SIGN")){
+                    // just count the number of math tuples
+                    formulaSize += 1f;
+                    math = true;
+                    token = "";
+                }else{
+                    if (math == true){
+                        // add the math formula size
+                        this.formulas.add(new Float(formulaSize));
+                        System.out.println("Formula size: " + formulaSize);
+                        formulaSize = 0f;
+                    }
+                    // now not a previous math term
+                    math = false;
+                    // assume a word
+                    if (!token.equals("")){
+                        this.words += 1f;
+                    }
+                    token = "";
+                }
+            }else{
+                // build up the token
+                token += Character.getName(character);
+                token = token.trim();
+            }
+        }
+        is.close();
+    }
+
+    public float getWordCount(){
+        return this.words;
+    }
+
+    public float averageFormulaSize(){
+        Float total = new Float(0);
+        for (Float formula : this.formulas){
+            total += formula;
+        }
+        Float average = total / new Float(this.formulas.size());
+        return average.floatValue(); 
+    }
+
+    public float maxFormulaSize(){
+        Float maxFormula = new Float(0);
+        for (Float formula : this.formulas){
+            if (formula >= maxFormula){
+                maxFormula = formula;
+            }
+            
+        }
+        return maxFormula.floatValue();
+    }
+}
---
+++ src/testing/index/TestFileStatstics.java
@@ -0,0 +1,61 @@
+package testing.index;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import index.FileStatistics;
+
+public class TestFileStatstics {
+    private Path folder;
+    @Before
+    public void setUp() throws Exception {
+        this.folder = Paths.get(System.getProperty("user.dir"), "resources", "test", "test_file_statistics");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testFile1() {
+        try {
+            Path testFile = Paths.get(this.folder.toString(), "1.html");
+            FileStatistics fs = new FileStatistics(new InputStreamReader(Files.newInputStream(testFile)));
+            System.out.println(fs.getWordCount());
+            assertEquals(fs.getWordCount() == 3065.0, true);
+            assertEquals(fs.averageFormulaSize() == 9.0, true);
+            assertEquals(fs.maxFormulaSize() == 17, true);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            fail("IOException raised");
+        }
+    }
+
+    @Test
+    public void testFile2() {
+        try {
+            Path testFile = Paths.get(this.folder.toString(), "2.html");
+            FileStatistics fs = new FileStatistics(new InputStreamReader(Files.newInputStream(testFile)));
+            assertEquals(fs.getWordCount() == 4385.0, true);
+            assertEquals(fs.averageFormulaSize() == 2.0, true);
+            assertEquals(fs.maxFormulaSize() == 3.0, true);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            fail("IOException raised");
+        }
+    }
+
+    
+}
