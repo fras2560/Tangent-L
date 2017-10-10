2370bae07ff6ae66cfedd07748021c50c0bd13ab
fras2560 <dallas_j_frase@hotmail.com>
2017-10-10 19:23:53 -0400

    Added some constants for Statistics fields

--- src/utilities/Constants.java
+++ src/utilities/Constants.java
@@ -4,6 +4,9 @@ public class Constants {
     public final static String WILDCARD = "*";
     public final static String ESCAPED_WILDCARD = "/*";
     public final static String FIELD = "contents";
-    public final static String NUMBER_OF_EQUATIONS = "equations"; 
-    public final static String NUMBER_OF_WORDS = "words";
+    public final static String FORMULA_COUNT = "number of formulas";
+    public final static String WORD_COUNT = "number of words";
+    public final static String MAX_FORMULA_SIZE = "max formula size";
+    public final static String AVERAGE_FORMULA_SIZE = "average formula size";
+    
 }
